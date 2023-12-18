65a3fc7460cde371f635a78067a2b090ae1d8e4a
Rahul Marban <rahulmarban@gmail.com>
2023-12-18 05:30:26 +0000

    avg grade

--- src/components/grade.jsx
+++ src/components/grade.jsx
@@ -18,14 +18,65 @@ export default function GradeCalculator() {
     { grade: 'O', internalMarks: 0, requiredMarks: 0 },
   ]);
 
+  const [avg, setAvg] = useState('O')
+
+  useEffect(() => {
+    let o = 0, a = 0, ap = 0, b = 0, bp = 0, c = 0;
+    courses.forEach((e) => {
+      switch(e.grade) {
+        case 'O':
+          o++
+          break;
+        case 'A':
+          a++
+          break;
+
+        case 'A+':
+          ap++
+          break;
+
+        case 'B':
+          b++
+          break;
+
+        case 'B+':
+          bp++
+          break;
+
+        case 'C':
+          c++
+          break;
+
+        default:
+          break;
+
+      }
+      
+      var tempObj = {
+        "O" : o,
+        "A" : a,
+        "A+" : ap,
+        "B" : b,
+        "B+" : bp,
+        "C": c
+       }
+
+       console.log(tempObj)
+       
+       var max  = Object.entries(tempObj).reduce((prev, current) => (prev[1] > current[1]) ? prev : current)[0];
+       setAvg(max)
+    })
+  }, [courses])
+
 
   return (
     <div id="grade" className="sub-card">
       <div className="cpga">
+        <h1>{avg}</h1>
         <p className="text">Grade</p>
       </div>
       <table className='grade-calc'>
-        <tr style={{display: 'flex', gap: 18}}>
+        <tr style={{ display: 'flex', gap: 18 }}>
           <th>S.no</th>
           <th>Internals</th>
           <th>Grade</th>
